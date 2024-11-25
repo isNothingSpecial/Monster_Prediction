890a5e4641222952715ef6837d50049131a7155e
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2024-11-25 14:41:11 +0700

    Update Processing.py

--- pages/Processing.py
+++ pages/Processing.py
@@ -81,7 +81,7 @@ if st.button("Cari Rekomendasi Monster"):
         st.write(f"Score: {score}")
         st.write(f"Alasan: {alasan}")
         # Menampilkan gambar monster, cek jika tidak ada gunakan default
-        image_path = f"{IMAGE_FOLDER}/{Monster}.webp"
+        image_path = f"{IMAGE_FOLDER}/{monster}.webp"
         if not os.path.exists(image_path):
             image_path = DEFAULT_IMAGE
-            st.image(image_path, caption=f"{Monster}", use_column_width=True)
+            st.image(image_path, caption=f"{monster}", use_column_width=True)
