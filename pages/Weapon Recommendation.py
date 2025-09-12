c9dc2943572aca843cf74ef1e319a331cdfe1ea9
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2025-09-12 12:44:34 +0700

    Update Weapon Recommendation.py

--- pages/Weapon Recommendation.py
+++ pages/Weapon Recommendation.py
@@ -81,12 +81,10 @@ if st.button("Dapatkan Rekomendasi Senjata"):
             recommendations = recommend_weapons(selected_monster, df2, df1)
 
         st.subheader(f"Rekomendasi Senjata untuk Melawan {selected_monster}:")
-        
-        else:
+
             for i, item in enumerate(recommendations):
                 weapon = item['senjata']
                 skor = item['skor']
-
                 with st.expander(f"{i+1}. {weapon['Nama Senjata']}"):
                     st.write(f"**Tipe Senjata:** {weapon['Tipe Senjata']}")
                     st.write(f"**Skor:** {skor:.0f}")
