d4fcb84d8815a93c0ce895fffd129d0ea48cbbf1
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2025-09-14 10:58:18 +0700

    Update Weapon Recommendation.py

--- pages/Weapon Recommendation.py
+++ pages/Weapon Recommendation.py
@@ -108,10 +108,9 @@ if st.button("Dapatkan Rekomendasi Senjata"):
                     st.write(f"**Skor:** {skor:.0f}")
 
                     st.markdown("---")
+                    st.write(f"**Attack Dasar:** {weapon['Attack Dasar']}"
+                    st.write(f"**Attack Max:** {weapon['Attack Max']}")
                     st.write(f"**Attack Max:** {weapon['Attack Max']}")
                     st.write(f"**Elemen:** {weapon['Elemen']} (Nilai: {weapon['Nilai Elemen']})")
-                    
-                    # --- PERUBAHAN DI SINI: MENAMBAHKAN % DI TAMPILAN ---
-                    st.write(f"**Critical:** {weapon['Critical']}%") 
-                    
+                    st.write(f"**Critical:** {weapon['Critical']}%")
                     st.write(f"**Skill:** {weapon['Skill']}")
